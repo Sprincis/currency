bf011ab4a242002e5c4679a7f3f2b4b00b029087
Jānis Sprincis <sprincis.janis@gmail.com>
2020-10-11 16:23:50 +0300

    Rest of the configuration files

--- Angular-project/src/app/app-routing.module.ts
+++ Angular-project/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CurrenciesComponent } from './currencies/currencies.component';
+import { SingleCurrencyComponent } from './single-currency/single-currency.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', redirectTo: '/currencies', pathMatch: 'full' },
+	{ path: 'currencies', component: CurrenciesComponent },
+	{ path: 'currency/:name', component: SingleCurrencyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular-project/src/app/app.module.ts
+++ Angular-project/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { SingleCurrencyComponent } from './single-currency/single-currency.component';
+import { CurrenciesComponent } from './currencies/currencies.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingleCurrencyComponent,
+    CurrenciesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPaginationModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  	{provide: LocationStrategy, useClass: PathLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
